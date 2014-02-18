fc154ca3fd1909ada935d67b48a74b1e5fcd5ed8
Ryan March <rmarch03@villanova.edu>
2014-02-18 00:10:19 -0500

    Thank you, Google

--- proj2/ch3.py
+++ proj2/ch3.py
@@ -59,56 +59,133 @@ def gen_list(num):
 
 
 def traceback_gene(final_matrix):
-	x = 1
-	y = 1
-	list_a = []
-	list_b = []
-	non_zero = True
-	x_over = False
-	y_over = False
-	while non_zero:
-		if x >= len(seq1):
-			x = len(seq1) + 0
-			x_over = True
-		if y >= len(seq2):
-			y = len(seq2) + 0
-			y_over = True
-		x_spot = len(seq1) - x
-		y_spot = len(seq2) - y
-		val = final_matrix[x_spot][y_spot]
-		diag = final_matrix[x_spot - 1][y_spot - 1]
-		horiz = final_matrix[x_spot - 1][y_spot]
-		vert = final_matrix[x_spot][y_spot - 1]
-		if val == diag + mismatch_penalty or val == diag + match_score:
-			list_a.append(seq1[x_spot])
-			list_b.append(seq2[y_spot])
-			x += 1
-			y += 1
-		else:
-			if val == horiz + gap_penalty:
-				list_a.append(seq1[x_spot])
-				if y_over:
-					list_b.insert(0, '-')
-				else:
-					list_b.append('-')
-				x += 1
-			else:
-				if val == vert + gap_penalty:
-					if x_over:
-						list_a.insert(0, '-')
-					else:
-						list_a.append('-')
-					list_b.append(seq2[y_spot])
-					y += 1
+	align_a = ""
+	align_b = ""
+	i = len(seq1) - 1
+	j = len(seq2) - 1
 
-		if len(seq1) <= x and len(seq2) <= y:
-			if x_over:
-				list_a.insert(0, '-')
-				list_b.append(seq2[y_spot])
-			if y_over:
-				list_a.append(seq1[x_spot])
-				list_b.insert(0, '-')
-			return [list_a, list_b]
+	while i > 0 and j > 0:
+		score = final_matrix[i][j]
+		diag = final_matrix[i - 1][j - 1]
+		vert = final_matrix[i][j - 1]
+		horiz = final_matrix[i - 1][j]
+
+		if score == diag + match_score or score == diag + mismatch_penalty:
+			align_a = seq1[i] + align_a
+			align_b = seq2[j] + align_b
+			i -= 1
+			j -= 1
+		elif score == horiz + gap_penalty:
+			align_a = seq1[i] + align_a
+			align_b = "-" + align_b
+			i -= 1
+		elif score == vert + gap_penalty:
+			align_a = "-" + align_a
+			align_b = seq2[j] + align_b
+			j -= 1
+		else:
+			print "You done did it"
+
+	while i > 0:
+		align_a = seq1[i] + align_a
+		align_b = align_b + "-"
+		i -= 1
+	while j > 0:
+		align_a = align_a + "-"
+		align_b = seq2[j] + align_b
+		j -= 1
+
+	align_a = seq1[0] + align_a
+	align_b = seq2[0] + align_b
+
+	return [align_a, align_b]
+
+
+# def traceback_gene(final_matrix):
+# 	x = 1
+# 	y = 1
+
+# 	list_a = []
+# 	list_b = []
+# 	list_path = []
+
+# 	non_zero = True
+# 	x_over = False
+# 	y_over = False
+	
+# 	while non_zero:
+# 		if x > len(seq1):
+# 			x = len(seq1) + 0
+# 			x_over = True
+# 		if y > len(seq2):
+# 			y = len(seq2) + 0
+# 			y_over = True
+
+# 		x_spot = len(seq1) - x
+# 		y_spot = len(seq2) - y
+
+# 		val = final_matrix[x_spot][y_spot]
+# 		diag = final_matrix[x_spot - 1][y_spot - 1]
+# 		horiz = final_matrix[x_spot][y_spot - 1]
+# 		vert = final_matrix[x_spot - 1][y_spot]
+
+# 		print "X", x_spot, seq1[x_spot]
+# 		print "Y", y_spot, seq2[y_spot]
+
+# 		if val == diag + mismatch_penalty or val == diag + match_score:
+# 			list_a.append(seq1[x_spot - 1])
+# 			list_b.append(seq2[y_spot - 1])
+# 			list_path.append('D')	
+# 			# print "ALIST", list_a[x - 1]
+# 			# print "BLIST", list_b[y - 1]
+# 			x += 1
+# 			y += 1
+# 		else:
+# 			if val == vert + gap_penalty:
+# 				list_a.append(seq1[x_spot - 1])
+# 				if y_over:
+# 					list_b.insert(0, '-')
+# 					list_path.insert(0, 'V')
+# 				else:
+# 					list_b.append('-')
+# 					list_path.append('V')
+# 				# print "ALIST", list_a[x - 1]
+# 				# print "BLIST", list_b[y - 1]
+# 				x += 1
+# 			else:
+# 				if val == horiz + gap_penalty:
+# 					if x_over:
+# 						list_a.insert(0, '-')
+# 						list_path.insert(0, 'H')
+# 					else:
+# 						list_a.append('-')
+# 						list_path.append('H')
+# 					list_b.append(seq2[y_spot - 1])
+# 					# print "ALIST", list_a[x - 1]
+# 					# print "BLIST", list_b[y - 1]
+# 					y += 1
+
+# 		if len(seq1) <= x and len(seq2) <= y:
+# 			if x_over:
+# 				list_a.insert(0, '-')
+# 				list_b.append(seq2[y_spot - 1])
+# 				list_path.insert(0, 'H')
+# 				print "XOVER Y", y_spot, seq2[y_spot - 1]
+# 			else:
+# 				if not x_over:
+# 					list_a.append(seq1[x_spot])
+# 			if y_over:
+# 				list_a.append(seq1[x_spot - 1])
+# 				#list_b.append(seq2[y_spot])
+# 				list_b.insert(0, '-')
+# 				list_path.insert(0, 'V')
+# 				print "YOVER X", x_spot, seq1[x_spot - 1]
+# 			else:
+# 				if not y_over:
+# 					list_b.append(seq2[y_spot])
+# 			# print "ALIST", list_a[x_spot]
+# 			# print "BLIST", list_b[y_spot]
+# 			return [list_a, list_b, list_path]
 
 # def traceback_gene(final_matrix, x, y):
 # 	list_a = []
@@ -167,9 +244,19 @@ for x in range(len(file_lista)):
 
 	print datmatrix
 	result_list = traceback_gene(datmatrix)
-	# result_list = traceback_gene(datmatrix, len(seq1) - 1, len(seq2) - 1)
 	print result_list
-	print len(result_list[0]), len(result_list[1])
+	# result_list = traceback_gene(datmatrix, len(seq1) - 1, len(seq2) - 1)
+	#print result_list
+	#print len(result_list[0]), len(result_list[1]), len(result_list[2])
+	
+	# result_seq1 = ''.join(result_list[0][::-1])
+	# result_seq2 = ''.join(result_list[1][::-1])
+	# result_path = ''.join(result_list[2][::-1])
+
+	# print result_seq1
+	# print result_seq2
+	# print "Path:", result_path
+
 	matrix.gen_html_table(datmatrix, filename)
 	# traceback_gene(datmatrix)
 
