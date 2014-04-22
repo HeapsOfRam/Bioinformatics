c84b098a44bbce82c8b15dc170d56436ab01bda3
Ryan March <rmarch03@villanova.edu>
2014-04-22 03:05:04 -0400

    Finish Project 5

--- proj5/orf.py
+++ proj5/orf.py
@@ -1,33 +1,30 @@
-# 1. READ IN INPUT
-# 2. SET READING POSITION AT FIRST NUCLEOTIDE (READING FRAME 1 - NUCLEOTIDE 0)
-# 3. BEGINNING AT THE STARTING POSITION, SEARCH IN TRIPLETS FOR AUG. IF FOUND, JUMP TO STEP 5. 
-# 	OTHERWISE, GO TO STEP 
-# 4. INCREMENT THE STARTING POSITION BY 1 (NEXT READING FRAME) IF THE STARTING POSITION IS
-#	< 3 , GO BACK TO STEP 3. OTHERWISE, YOU HAVE NOT FOUND AN ORF. PRINT NOT FOUND
-# 5. ONCE YOU HAVE FOUND A START CODON, SEARCH IN TRIPLETS WITH THE SAME READING FRAME AND LOOK
-#	FOR A STOP CODON. IF A STOP CODON IS FOUND AND THE LENGTH IS >= 60, YOU HAVE FOUND ACCEPTABLE
-#	ORF. PRINT RESULTS. IF STOP CODON NOT FOUND, RETURN TO STEP 4
+# Ryan March, Vincent Eaton, Anita Uche
 
 def orf_check(dna_strand):
-	global orf_found
+	global orf_count
 
 	orf_length = 0
 	orf = ""
+	base_orf = ""
 
 	for i in range (0, len(dna_strand), 3):
 		orf_length += 3
 
 		if dna_strand[i:i + 3].upper() in stop_codons and orf_length >= minimum_length:
-			orf_found = True
-			orf += dna_strand[i: i + 3]
+			orf_count += 1
+			orf += dna_strand[i:i + 3]
+			base_orf = dna_strand[i:i + 3]
 			print "ORF FOUND in " + in_file_base + str(iteration)
-			out_file.write(orf)
-			out_file.close()
+			out_file.write(orf.upper() + '\n')
+			return [True, orf]
 
 		orf += dna_strand[i:i + 3] + "-"
+		base_orf += dna_strand[i:i + 3]
 
-start_codon = "AUG"
-stop_codons = ["UAA", "UAG", "UGA"]
+	return [False, orf]
+
+start_codons = ["AUG", "ATG"]
+stop_codons = ["UAA", "TAA", "UAG", "TAG", "UGA", "TGA"]
 minimum_length = 60
 
 in_file_base = "strand_"
@@ -35,35 +32,40 @@ in_file_ext = ".txt"
 out_file_base = "result_"
 out_file_ext = ".txt"
 
-for iteration in range(1, 6):
+for iteration in range(1, 7):
 	in_file = open(in_file_base + str(iteration) + in_file_ext, 'r')
 	out_file = open(out_file_base + str(iteration) + out_file_ext, 'w')
-	orf_found = False
+	orf_count = 0
+	covered = False
 
 	in_file.readline()
 
 	strand = in_file.readline()
 	reverse_strand = strand[::-1]
 
-	for x in range(3):
-		#print str(orf_found) + str(iteration) + '1'
+	for x in range(len(strand) - 4):
+		if strand[x:x + 3].upper() in start_codons:
+			results = orf_check(strand[x:])
 
-		if orf_found != True:
-			if strand[x:x + 3].upper() == start_codon:
-				orf_check(strand[x:])
+		covered = results[0]
 
-		#print str(orf_found) + str(iteration) + '2'
+		if covered:
+			x += len(results[1])
+			covered = False
 
-		if orf_found != True:
-			if reverse_strand[x:x + 3].upper() == start_codon:
-				orf_check(reverse_strand[x:])
+		if reverse_strand[x:x + 3].upper() in start_codons:
+			results = orf_check(reverse_strand[x:])
 
-		#print str(orf_found) + str(iteration) + '3'	
+		covered = results[0]
 
+		if covered:
+			x += len(results[1])
+			covered = False
 
 	in_file.close()
 
-	if orf_found != True:
+	if orf_count == 0:
 		print "No ORF Found in " + in_file_base + str(iteration)
 		out_file.write("None")
-		out_file.close()
+	
+	out_file.close()
